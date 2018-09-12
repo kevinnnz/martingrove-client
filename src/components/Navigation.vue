2fc0d92a879fd9329c0f0c02fd19f10302c400cd
Kevin Z <>
2018-09-11 22:34:32 -0400

    Header and nav structure styled.

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="header">
-    <h1>Martin Grove Golf Course</h1>
+    <div class="row">
+      <div class="col s8">
+        <h1>Martin Grove Golf Course</h1>
+      </div>
+      <div class="col s4">
+        <h2 class="cartlabel" style="font-size: 16px;">My Order</h2>
+        <h2 class="cart">0</h2>
+      </div>
+    </div>
     <nav>
-      <router-link v-for="routes in links" 
-      v-bind:key="routes.id"
-      :to="`${routes.page}`">{{routes.text}}</router-link>
+      <router-link v-for="routes in links" v-bind:key="routes.id" :to="`${routes.page}`" class="nav-link">{{routes.text}}</router-link>
     </nav>
   </div>
 </template>
