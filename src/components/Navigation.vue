7d65e952418c5a7694e94de8313c50748a65c501
George Young <gy674600@gmail.com>
2018-09-21 12:48:45 -0400

    Updated Nav

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,10 +1,9 @@
 <template>
-<div>
-  <!-- <div class="header">
+  <div class="header">
     <div class="row masthead">
       <div class="col s8">
       <img src="../assets/Club_Logo-NoBG.png" class="logo"/>
-      <h1>Martin Grove Golf Course</h1>
+      <!-- <h1>Martin Grove Golf Course</h1> -->
       </div>
       <div class="col s4 cart">
         <h2 class="cartlabel" style="font-size: 16px;">My Order</h2>
@@ -15,7 +14,7 @@
       <router-link v-for="routes in links" v-bind:key="routes.id" :to="`${routes.page}`" class="nav-link">{{routes.text}}</router-link>
     </nav> 
     </div>
-   -->
+   <!-- <div>
   <header class="header">
       <nav class="inner">
         <img class="logo" src="../assets/Club_Logo-BG.png" alt="logo">
@@ -26,7 +25,7 @@
     <transition name="fade" mode="out-in">
       <router-view class="view"></router-view>
     </transition>
-    </div>
+    </div> -->
 </template>
 
 <script>
@@ -71,94 +70,3 @@ export default {
   }
 }
 </script>
-
-<style>
-body {
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-  font-size: 15px;
-  background-color: #f2f3f5;
-  margin: 0;
-  padding-top: 55px;
-  color: #34495e;
-  overflow-y: scroll;
-}
-a {
-  color: #34495e;
-  text-decoration: none;
-}
-.header {
-  background-color: green;
-  position: fixed;
-  z-index: 999;
-  height: 55px;
-  top: 0;
-  left: 0;
-  right: 0;
-}
-.header .inner {
-  max-width: 900px;
-  box-sizing: border-box;
-  margin: 0px auto;
-}
-.header a {
-  color: rgba(255,255,255,0.8);
-  line-height: 24px;
-  transition: color 0.15s ease;
-  display: inline-block;
-  vertical-align: middle;
-  font-weight: 300;
-  letter-spacing: 0.075em;
-  margin-right: 1.8em;
-}
-.header a:hover {
-  color: beige;
-}
-.header a.router-link-active {
-  color: #fff;
-  font-weight: 400;
-}
-.header a:nth-child(6) {
-  margin-right: 0;
-}
-.header .github {
-  color: #fff;
-  font-size: 0.9em;
-  margin: 0;
-  float: right;
-}
-.logo {
-  width: 50px;
-  margin-right: 100px;
-  display: inline-block;
-  vertical-align: middle;
-}
-.view {
-  max-width: 800px;
-  margin: 0 auto;
-  position: relative;
-}
-.fade-enter-active,
-.fade-leave-active {
-  transition: all 0.2s ease;
-}
-.fade-enter,
-.fade-leave-active {
-  opacity: 0;
-}
-@media (max-width: 860px) {
-  .header .inner {
-    padding: 15px 30px;
-  }
-}
-@media (max-width: 600px) {
-  .header .inner {
-    padding: 15px;
-  }
-  .header a {
-    margin-right: 1em;
-  }
-  .header .github {
-    display: none;
-  }
-}
-</style>
